ae1ccc02c1507bacc40fe9e1af0a2e2ecceb212c
Mike Burns <mgburns@bu.edu>
2012-03-28 17:15:31 -0400

    Modified setup file to include static/template files on install

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
 	name='bu-django-admin',
 	version='0.1.0',
 	author='Tyler Wiest',
 	author_email='jtwiest@bu.edu',
-	packages=['django_buadmin',],
+	packages=find_packages(),
+	include_package_data=True,
 	url='http://github.com/bu-ist/bu-django-admin',
 	license='LICENSE.txt',
 	description='A BU branded skin for the built-in Django Admin',
